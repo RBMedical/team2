b51457310dcf365d92c06f4bcfce90c23e95b217
RBMedical <rbcheckup@gmail.com>
2026-06-17 06:20:54 +0700

    Update api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,5 @@
 const APP_SCRIPT_URL =
-  "https://script.google.com/macros/s/AKfycbxDE6oKmh7wUujo2tpVXqiDAiHHgsePH5O-0W01K44XMVNZC1KdUcH9b4q8l384XsRO/exec";
+  "https://script.google.com/macros/s/AKfycbyUr2AWTlar8StCHwPHtSQYO2MBflJlH7HiOM5nrFB_25l6PrAHiW89H7xCbdqaA2Gi2A/exec";
 
 // Global loading counter
 let _loadingCount = 0;
